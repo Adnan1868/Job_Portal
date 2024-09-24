31ee5fbb66d6c3d7ed996573a99e7efec0516845
Abdullah Adnan Abul Kalam <108984092+Adnan1868@users.noreply.github.com>
2024-09-24 10:38:33 +0600

    Update jobSeeker_Controller.php

--- JobPortal/controllers/jobSeeker_Controller.php
+++ JobPortal/controllers/jobSeeker_Controller.php
@@ -9,7 +9,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $gender = $_POST['gender'];
     $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
-    // Validation (Regex for email)
     if (preg_match("/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/", $email)) {
         $stmt = $pdo->prepare("INSERT INTO job_seekers (full_name, email, contact_no, gender, password) VALUES (?, ?, ?, ?, ?)");
         $stmt->execute([$full_name, $email, $contact_no, $gender, $password]);
