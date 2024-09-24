4c69bbe3793afc33b61f27c0f7084b3963623356
Abdullah Adnan Abul Kalam <108984092+Adnan1868@users.noreply.github.com>
2024-09-24 10:38:13 +0600

    Update employerController.php

--- JobPortal/controllers/employerController.php
+++ JobPortal/controllers/employerController.php
@@ -8,7 +8,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $contact_no = $_POST['contact_no'];
     $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
-    // Validation (Regex for email)
     if (preg_match("/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/", $email)) {
         $stmt = $pdo->prepare("INSERT INTO employers (company_name, email, contact_no, password) VALUES (?, ?, ?, ?)");
         $stmt->execute([$company_name, $email, $contact_no, $password]);
